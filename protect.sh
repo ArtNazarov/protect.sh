3476fc22593a6c24d623354c2bc8e8f1d6ed18ef
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-05-04 18:35:14 +0500

    Create protect.sh

---
+++ protect.sh
@@ -0,0 +1,29 @@
+function protect(){
+	echo "apply protection"
+	sudo chattr +i -R "$1"
+}
+function unprotect(){
+	echo "apply unprotection"
+	sudo chattr -i -R "$1"
+}
+
+if  [ $# -lt 2 ] ; then
+	echo " need 2 args command and path "
+fi
+
+action=$1
+path="$2"
+echo "Param count is $#"
+echo "Chosen action is $action"
+echo "Chosen path is $path"
+
+case "$action" in
+	"protect" )
+		protect "$path"
+	;;
+	"unprotect" )
+		unprotect "$path"
+	;;
+	*)
+		echo "commands protect or unprotect require"
+esac
