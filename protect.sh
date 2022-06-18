f054958d9f4f79f35c5b28ec667dfc15116cafb5
ArtNazarov <33857419+ArtNazarov@users.noreply.github.com>
2022-06-18 19:38:20 +0500

    Update protect.sh

--- protect.sh
+++ protect.sh
@@ -1,15 +1,17 @@
+  GNU nano 6.3                                                                           /home/artem/protect.sh                                                                                     
 function protect(){
-	echo "apply protection"
-	sudo chattr +i -R "$1"
-}
-function unprotect(){
-	echo "apply unprotection"
-	sudo chattr -i -R "$1"
+echo "apply protection"
+sudo chattr +i -R "$1"
 }
 
-if  [ $# -lt 2 ] ; then
-	echo " need 2 args command and path "
-fi
+function unprotect(){
+echo "apply unprotection"
+sudo chattr -i -R "$1"
+}
+
+if [ $# -lt 2 ] ; then
+echo " need 2 args command and path "
+else
 
 action=$1
 path="$2"
@@ -18,12 +20,14 @@ echo "Chosen action is $action"
 echo "Chosen path is $path"
 
 case "$action" in
-	"protect" )
-		protect "$path"
-	;;
-	"unprotect" )
-		unprotect "$path"
-	;;
-	*)
-		echo "commands protect or unprotect require"
+"protect" )
+protect "$path"
+;;
+"unprotect" )
+unprotect "$path"
+;;
+*)
+echo "commands protect or unprotect require"
 esac
+
+fi
