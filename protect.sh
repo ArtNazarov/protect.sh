d27e9afa78e10e4187f482d3cd0922d5059c5e74
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2023-09-08 19:59:49 +0500

    Update protect.sh

--- protect.sh
+++ protect.sh
@@ -1,5 +1,8 @@
 function protect(){
 	echo "apply protection"
+	read -p "Input password: " pass
+	echo -n $pass | md5sum > md5.sum
+	mv md5.sum $1
 	sudo find "$1" -type d -exec chmod 700 {} \; 
 	sudo find "$1" -type f -exec chmod 600 {} \;
 	sudo chown root:root "$1" -R
@@ -7,10 +10,24 @@ function protect(){
 }
 function unprotect(){
 	echo "apply unprotection"
-	sudo chattr -i -R "$1"
-	sudo find "$1" -type d -exec chmod 755 {} \; 
-	sudo find "$1" -type f -exec chmod 644 {} \;
-	sudo chown $USER:$USER "$1" -R	
+	read -p "Input password for folder: " pass
+	tst=$(echo -n $pass | md5sum )
+	sudo cp "$1/md5.sum" ./md5.sum
+	sudo chmod 755 md5.sum
+	sudo chown $USER:$USER md5.sum
+	estr=$(cat md5.sum)
+	sudo rm ./md5.sum
+	if [[ $tst == $estr ]]; then
+		echo "Okay, confirmed"
+		sudo chattr -i -R "$1"
+		sudo find "$1" -type d -exec chmod 755 {} \;
+		sudo find "$1" -type f -exec chmod 644 {} \;
+		sudo chown $USER:$USER "$1" -R
+	else
+		echo "Fail, mismatch"
+	fi
+
+
 }
 
 if  [ $# -lt 2 ] ; then
